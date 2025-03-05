70286d82d6a969ff217c2369af8dc24e07c8a97c
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-03-05 15:27:48 +0700

    feat: Hello world express, sanity check

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+import express from "express";
+
+const app = express();
+
+app.get('/test', (req, res) => {
+    res.json({'hello': 'world'})
+})
+
+app.listen(3000, () => {
+    console.log("Running at localhost:3000")
+})
