39c2047fb7faa57862795b2ee28dcfdf62da569f
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-03-08 22:37:38 +0700

    chore: change indentation to 2 spaces

--- index.ts
+++ index.ts
@@ -8,5 +8,5 @@ import app from "./app";
 const PORT = process.env.SERVER_PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
+  console.log(`Server running at http://localhost:${PORT}`)
 })
